d0388252a4ba424321ff4dcf674c9cddd2aef9f9
takanuva15 <takanuva15@users.noreply.github.com>
2021-04-11 18:57:10 -0400

    #5: Move bash pre-checks into new steps in action.yml and provide option to give custom token (#6)
    * #5: Separated bash script into steps within action.yml
    
    * #5: Provided an optional token input to provide a custom token for the action to use

--- README.md
+++ README.md
@@ -22,11 +22,13 @@ jobs:
 Commit the file and create a PR to `main` - you should see the action run.
 
 ## Additional Usage Parameters
-There are two additional parameters you can specify for the GitHub action:
+There are three additional parameters you can specify for the GitHub action:
 - `excused_label`: A label that, when added to the PR, will exempt the PR from needing to satisfy the changelog check.
   - Default is `changelog update optional`
 - `changelog_filename`: The name of the changelog file. The GitHub Action will verify whether this file has been updated within the PR.
   - Default is `CHANGELOG.md`
+- `token`: The authentication token to use for accessing the PR's information. A token is required in order to view the files changed in a PR.
+  - Default is `"${{ github.token }}"`
 
 (Both parameters are case-sensitive)
 
@@ -38,7 +40,11 @@ steps:
     with:
       excused_label: my_custom_label
       changelog_filename: changelog.txt
+      token: ${{ secrets.MY_TOKEN }}
 ```
+
+## Notes
+If you add a label after a PR has already been created, you will need to trigger a fresh build by pushing (or force-pushing) a commit. (This is due to GitHub caching the PR data when the PR is created, so a commit is required in order to refresh the cache)
 
 ## Contributing
 We are happy to take contributions. If you experience an issue or see something that can be improved, feel free to raise an issue. Assuming that there is agreement on the issue, fork the repo and raise a PR to make your change.
--- verify_changelog_updated.sh
+++ verify_changelog_updated.sh
@@ -4,29 +4,9 @@
 echoerr() { printf "%s\n" "$*" >&2; }
 
 #credit to https://stackoverflow.com/a/61922402 for providing a starting point for this script
-github_context_info_json=$(cat "$GITHUB_EVENT_PATH")
 
-# Step 1: Check that the execution context is a PR
-if ! jq --exit-status 'has("pull_request")' <<<"$github_context_info_json" >/dev/null; then
-  echoerr "PR info could not be found! This action should only be run in the context of a PR. Please verify this " \
-    "action is only being triggered on 'pull_request'. (See this action\"s README for more info)"
-  exit 1
-fi
-
-# Step 2: Check that the PR doesn't have an excused label to ignore the changelog check
-pr_info_json=$(jq --raw-output '.pull_request' <<<"$github_context_info_json")
-pr_labels=$(jq -r '.labels[] | .name' <<<"$pr_info_json")
-
-#function obtained from https://stackoverflow.com/a/15394738
-# shellcheck disable=SC2199
-if [[ ${pr_labels[@]} =~ ${EXCUSED_LABEL} ]]; then
-  echo "PR has label '$EXCUSED_LABEL', so no changelog update is required."
-  exit 0
-fi
-
-# Step 3: Fetch all changed files in this PR and verify the changelog file is among the changes
-pr_api_url=$(jq -r '._links.self.href' <<<"$pr_info_json")
-pr_files_json=$(curl -s -u "$OWNER":"$GITHUB_TOKEN" -H "Accept: application/vnd.github.v3+json" "$pr_api_url/files")
+# Fetch all changed files in this PR and verify the changelog file is among the changes
+pr_files_json=$(curl -s -u "$OWNER":"$GITHUB_TOKEN" -H "Accept: application/vnd.github.v3+json" "$PR_API_URL/files")
 pr_files=$(jq -r '.[] | .filename' <<<"$pr_files_json")
 
 # shellcheck disable=SC2199
